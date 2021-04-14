989c9d9e9ff6f2e35db34fc402fa69a1ab0b0945
elgalloloco <zreggert@gmail.com>
2021-04-14 12:10:54 -0400

    added questions to the questions array

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquire');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions for your project.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How do you use this application?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines for your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for you project?',
+        name: 'test',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
